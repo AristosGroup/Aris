f527206f87809804a6ca8cb5852681315b7f8aa0
Aleksey Kuznetsov <mrakobesov@gmail.com>
2013-04-11 18:46:18 +0400

    BOOTSTRAP layout

--- app/protected/views/layouts/column2.php
+++ app/protected/views/layouts/column2.php
@@ -1,22 +1,88 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
-<div class="span-19">
-	<div id="content">
-		<?php echo $content; ?>
-	</div><!-- content -->
-</div>
-<div class="span-5 last">
-	<div id="sidebar">
-	<?php
-		$this->beginWidget('zii.widgets.CPortlet', array(
-			'title'=>'Operations',
-		));
-		$this->widget('zii.widgets.CMenu', array(
-			'items'=>$this->menu,
-			'htmlOptions'=>array('class'=>'operations'),
-		));
-		$this->endWidget();
-	?>
-	</div><!-- sidebar -->
-</div>
+
+
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="span2">
+            <div class="sidebar-nav">
+                <ul class="nav nav-list">
+                    <li class="nav-header">Задачи</li>
+                    <li class="active"><a href="#"><i class="icon-envelope"></i> Новые <span class="badge">5</span></a>
+                    </li>
+                    <li><a href="#"><i class="icon-star"></i>Избранные</a></li>
+                    <li><a href="#">Созданные мной</a></li>
+                    <li><a href="#">Назначенные мне</a></li>
+
+                    <li class="nav-header">Фильтры</li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+
+                    <li class="nav-header">Проекты</li>
+                    <li><a href="#">Philips</a></li>
+                    <li><a href="#">Oursson</a></li>
+
+
+                </ul>
+            </div>
+        </div>
+        <!--/span-->
+        <div class="span10">
+
+            <div class="row-fluid issue-toolbar">
+
+
+
+
+                    <div class="span6">
+                        <div class="btn-toolbar">
+                        <a class="btn"  rel="tooltip" title="first tooltip" href="#"><i class="icon-check"></i></a>
+
+                        <div class="btn-group">
+                            <button rel="tooltip" title="first tooltip" class="btn">Left</button>
+                            <button rel="tooltip" title="first tooltip" class="btn">Middle</button>
+                            <button rel="tooltip" title="first tooltip" class="btn">Right</button>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="span4 offset2">
+                        <div class="btn-group">
+                            <button class="btn"><i class="icon-list"></i></button>
+                            <button class="btn"><i class="icon-th"></i></button>
+                            <button class="btn"><i class="icon-tasks"></i></button>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            <div class="row-fluid">
+                <table>
+
+                    <tr>
+                        <td></td>
+                    </tr>
+
+                </table>
+
+            </div>
+            <!--/row-->
+
+        </div>
+        <!--/span-->
+    </div>
+    <!--/row-->
+
+    <hr>
+
+    <footer>
+        <p>&copy; Company 2013</p>
+    </footer>
+
+</div><!--/.fluid-container-->
+
 <?php $this->endContent(); ?>
+
+
+
--- app/protected/views/site/login.php
+++ app/protected/views/site/login.php
@@ -5,7 +5,7 @@
 
 $this->pageTitle=Yii::app()->name . ' - Login';
 $this->breadcrumbs=array(
-	'Login',
+    'Login',
 );
 ?>
 
@@ -14,40 +14,34 @@ $this->breadcrumbs=array(
 <p>Please fill out the following form with your login credentials:</p>
 
 <div class="form">
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'login-form',
-	'enableClientValidation'=>true,
-	'clientOptions'=>array(
-		'validateOnSubmit'=>true,
-	),
-)); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+    <?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+        'id'=>'login-form',
+        'type'=>'horizontal',
+        'enableClientValidation'=>true,
+        'clientOptions'=>array(
+            'validateOnSubmit'=>true,
+        ),
+    )); ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username'); ?>
-		<?php echo $form->error($model,'username'); ?>
-	</div>
+    <p class="note">Fields with <span class="required">*</span> are required.</p>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?>
-		<p class="hint">
-			Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
-		</p>
-	</div>
+    <?php echo $form->textFieldRow($model,'username'); ?>
 
-	<div class="row rememberMe">
-		<?php echo $form->checkBox($model,'rememberMe'); ?>
-		<?php echo $form->label($model,'rememberMe'); ?>
-		<?php echo $form->error($model,'rememberMe'); ?>
-	</div>
+    <?php echo $form->passwordFieldRow($model,'password',array(
+        'hint'=>'Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>',
+    )); ?>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Login'); ?>
-	</div>
+    <?php echo $form->checkBoxRow($model,'rememberMe'); ?>
 
-<?php $this->endWidget(); ?>
+    <div class="form-actions">
+        <?php $this->widget('bootstrap.widgets.TbButton', array(
+            'buttonType'=>'submit',
+            'type'=>'primary',
+            'label'=>'Login',
+        )); ?>
+    </div>
+
+    <?php $this->endWidget(); ?>
+
 </div><!-- form -->
